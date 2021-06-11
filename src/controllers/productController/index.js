32f2f88f78b8c93e66cb22f184c5521556222cc8
Kartik Mahato <kartikmahato1996@gmail.com>
2021-06-11 19:59:03 +0530

    token expiry changed to 24h

--- src/controllers/productController/index.js
+++ src/controllers/productController/index.js
@@ -4,9 +4,11 @@ const Product = require('../../models/Product');
 exports.createProduct = async (req, res) => {
     // res.status(200).json({ file: req.files, body: req.body });
     try {
-        const { name, price, description, category, createdBy, quantity } = req.body;
+        const { name, price, description, category, quantity } = req.body;
 
         let productPictures = [];
+        console.log(req.body);
+        
         if (req.files.length > 0) {
             productPictures = req.files.map(file => {
                 return { img: file.filename };
