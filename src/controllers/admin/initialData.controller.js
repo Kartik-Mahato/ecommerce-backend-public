ba952b7ac53e3fca60a6cb476dea34e6abe05bec
Kartik Mahato <kartikmahato1996@gmail.com>
2021-06-12 13:08:23 +0530

    bug fix in initialData controller

--- src/controllers/admin/initialData.controller.js
+++ src/controllers/admin/initialData.controller.js
@@ -6,10 +6,33 @@ exports.initialData = async (req, res) => {
         const categories = await Category.find().select("-updatedAt -__v");
         const products = await Product.find().select("-createdAt -updatedAt -__v");
         res.status(200).json({
-            categories,
+            categories: createCategory(categories),
             products
         });
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
 }
+
+function createCategory(categories, parentId = null) {
+    const categoryList = [];
+    let category;
+
+    if (parentId === null) {
+        category = categories.filter(category => category.parentId == undefined);
+    } else {
+        category = categories.filter(category => category.parentId == parentId);
+    }
+
+    for (let cat of category) {
+        categoryList.push({
+            _id: cat._id,
+            name: cat.name,
+            slug: cat.slug,
+            parentId: cat.parentId,
+            children: createCategory(categories, cat._id)
+        })
+    }
+
+    return categoryList;
+};
