faef4827e4d5ee99be4d8fd8c475da4492d4524f
Kartik Mahato <kartikmahato1996@gmail.com>
2021-06-17 12:17:56 +0530

    modification in user controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -55,12 +55,16 @@ exports.signin = async (req, res) => {
             return res.status(403).json({ message: "Incorrect Password." });
         }
 
+        if (user.role !== 'user') {
+            res.status(403).json({ message: "Forbidden" })
+        }
+
         const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
 
         return res.status(200).json({
             message: "Login Success",
             token,
-            userId: user._id
+            user
         });
     } catch (error) {
         return res.status(500).json({ error: error.message });
